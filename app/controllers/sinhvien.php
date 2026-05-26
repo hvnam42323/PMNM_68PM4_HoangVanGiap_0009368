1f564c665f71820064cbde083efba89e991ca50d
hoang nam <dhoangvanhu@gmail.com>
2026-05-26 23:11:10 +0700

    Hien thi danh sach sinh vien

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -1,13 +1,16 @@
 <?php
-class home {
-    public function index(){
-        echo "Day la trang chu";
+require_once '../app/models/sinhvienModel.php';
+require_once '../app/core/Controller.php';
+class sinhvien extends Controller {
+    public function index() {
+        $sinhvienModel = $this->model('sinhvienModel');
+        $sinhvien = $sinhvienModel->getALLSinhVien();
+
+        $this->view("sinhvien/index", ["sinhvien" => $sinhvien, "title" => "Danh sach sinh vien"]);
     }
-    public function create(){
-        echo "Day la trang tao moi";
-    }
-    public function login(){
-        require_once '../app/views/auth/Login.php';
+    public function create() {
+        // trả về view
+        require_once '../app/views/sinhvien/create.php';
     }
 }
 ?>
