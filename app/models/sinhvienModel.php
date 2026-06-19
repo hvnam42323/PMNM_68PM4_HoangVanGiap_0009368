0dfa114f85b0648fc28fa5024864587ce04139da
hoang nam <dhoangvanhu@gmail.com>
2026-06-19 22:27:01 +0700

    feat<lophoc>: CRUD lophoc

--- app/models/sinhvienModel.php
+++ app/models/sinhvienModel.php
@@ -6,13 +6,13 @@
             $this->conn = $db->connect();
         }
         public function getALLSinhVien() {
-            $query = "SELECT * FROM tbl_sinhvien";
+            $query = "SELECT * FROM sinhviens";
             $stmt = $this->conn->prepare($query);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
-         public function create($hoten, $gioitinh, $mssv) {
-            $query = "INSERT INTO tbl_sinhvien (sinhvien, giotinh, mssv) VALUES (:hoten, :gioitinh, :mssv)";
+        public function create($hoten, $gioitinh, $mssv) {
+            $query = "INSERT INTO sinhviens (hoten, giotinh, mssv) VALUES (:hoten, :gioitinh, :mssv)";
             $stmt = $this->conn->prepare($query);
             $stmt->bindParam(':hoten', $hoten);
             $stmt->bindParam(':gioitinh', $gioitinh);
@@ -22,6 +22,52 @@
             } else {
                 return false;
             }
-         }
+        }
+
+        public function paging($limit, $offset, $search = '') {
+            // đếm tổng số bản ghi
+            $sqlCount = "SELECT COUNT(*) as total FROM sinhviens";
+            $stmtCount = $this->conn->prepare($sqlCount);
+            $stmtCount->execute();
+            $totalRecord = $stmtCount->fetchColumn();
+            $totalRecord = ceil($totalRecord / $limit);
+
+
+            $sql = "SELECT * FROM sinhviens LIMIT :limit OFFSET :offset";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+            $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return [
+                'sinhviens' => $result,
+                'totalpage' => $totalRecord
+            ];
+        }
+
+        public function getById($id) {
+            $query = "SELECT * FROM sinhviens WHERE id = :id";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+
+        public function update($id, $hoten, $gioitinh, $mssv) {
+            $query = "UPDATE sinhviens SET hoten = :hoten, giotinh = :gioitinh, mssv = :mssv WHERE id = :id";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':hoten', $hoten);
+            $stmt->bindParam(':gioitinh', $gioitinh);
+            $stmt->bindParam(':mssv', $mssv);
+            return $stmt->execute();
+        }
+        public function delete($id) {
+            $query = "DELETE FROM sinhviens WHERE id = :id";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':id', $id);
+            return $stmt->execute();
+        }
     }
 ?>
